250ec0fcb14418a95f302fe76dcffee524196fc8
Shiv Bhonde <shivbhonde04@gmail.com>
2021-10-19 11:15:14 +0530

    Revert "setup routes and models"
    This reverts commit 6ab9e12e5a07d8fd7acd8323d6b8f06ac51dcf70.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 // import Cart from './pages/Cart';
 import HomePage from './pages/HomePage';
 // import ProductList from './pages/ProductList';
-import Product from './pages/Product';
+// import Product from './pages/Product';
 // import Register from './pages/Register';
 // import Login from './pages/Login';
 export default function App() {
-	return <Product />;
+	return <HomePage />;
 }
